0f53c98b61e7acc42ceaa53ef2a013290b992213
P Chatman <34121076+pchatman@users.noreply.github.com>
2023-11-08 18:45:46 -0500

    Add files via upload
    chore: first commit

---
+++ javaHaiku/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+    //Begin Main Method
+    public static void main(String[] args) {
+        //Print statements
+        System.out.println("This is my Haiku");
+        System.out.println("This is a Java Haiku");
+        System.out.println("With Five Seven Five");
+        System.out.print("That means it has 5 syllables, ");
+        System.out.println("then 7 syllables, then 5 syllables yet again.");
+
+        //Math formula
+        int numSymbol = (5 + 5 + (3 * 2) + 1);
+        System.out.println("That means there are a total of " + numSymbol
+                + " syllables in a Haiku.");
+
+    } //End Main Method
+}
